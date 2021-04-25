85df544c298a07cd1ae35e5fab77990789cf4844
tyagi-iiitv <anjul.ten@gmail.com>
2021-04-25 16:36:49 -0700

    Search space is now updated with network drag

--- src/SearchSpace.js
+++ src/SearchSpace.js
@@ -6,26 +6,47 @@ const equals = (a,b) =>
     a.every((v,i) => v === b[i]);
 
 export default class SearchSpace extends Component {
+    constructor(props){
+        super(props);
+        this.state = {emb: []}
+        this.generateData = this.generateData.bind(this);
+    }
+
+    generateData(nodeIds){
+        // let numNodes = nodeIds.length;
+        let numPoints = 200;
+        let emb = new Array(numPoints);
+        for(let i=0;i<numPoints;i++){
+            let curDict = {
+                x: Math.random(), 
+                y: Math.random(),
+            };
+            emb[i] = curDict;
+        }
+        this.setState({emb: emb});
+    }
+
     componentDidUpdate(prevProps){
         if(prevProps.nodes && this.props.nodes && !equals(prevProps.nodes, this.props.nodes)){
-            console.log("now search space will change");
+            this.generateData(this.props.nodes);
         }
     }
     render() {
         // if(this.props.emb.length === 0){
         //     return <div/>
         // }
+        console.log(this.state, "Inside render")
         return (
             <div>
-                {/* <Plot
+                <Plot
                 data={[
                     {   
                         //Creates the original scatterplot based on the embeddings
-                        x: this.props.emb.map(({x}) => x),
-                        y: this.props.emb.map(({y}) => y),
+                        x: this.state.emb.map(({x}) => x),
+                        y: this.state.emb.map(({y}) => y),
                         type: 'scatter',
                         mode: 'markers',
-                        text: this.props.emb.map(({mask}) => mask),
+                        // text: this.state.emb.map(({mask}) => mask),
                         marker: {color: '#F08080', size: 10},
                     },
                 ]}
@@ -40,7 +61,7 @@ export default class SearchSpace extends Component {
                             plot_bgcolor: "transparent",
                             title: "Search Space"
                         }}
-                    />  */}
+                    /> 
             </div>
 
         );
