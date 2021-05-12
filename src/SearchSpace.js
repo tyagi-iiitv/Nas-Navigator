a2bb9687fae00bc087828056510c32b4b1da2176
tyagi-iiitv <anjul.ten@gmail.com>
2021-05-11 18:20:32 -0700

    Scatterplot drag now works

--- src/SearchSpace.js
+++ src/SearchSpace.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Plot from 'react-plotly.js';
+const skmeans = require('skmeans');
 
 const equals = (a,b) => 
     a.length === b.length &&
@@ -8,7 +9,7 @@ const equals = (a,b) =>
 export default class SearchSpace extends Component {
     constructor(props){
         super(props);
-        this.state = {emb: []}
+        this.state = {emb: [], data: [], updated: false};
         this.generateData = this.generateData.bind(this);
     }
 
@@ -16,20 +17,38 @@ export default class SearchSpace extends Component {
         let numNodes = nodeIds.length;
         let numPoints = 200;
         let emb = new Array(numPoints);
+        let data = new Array(numPoints);
+        for(let i=0;i<numPoints;i++){
+            let x = Math.random();
+            let y = Math.random();
+            data[i] = [x, y];
+        }
+        let res = skmeans(data, 6, null, 10);
         for(let i=0;i<numPoints;i++){
             let curDict = {
-                x: Math.random(), 
-                y: Math.random(),
+                x: data[i][0],
+                y: data[i][1],
                 mask: Math.floor(Math.random()*Math.pow(2,numNodes-1)).toString(2).padStart(numNodes,0),
-            };
+                cluster: res.idxs[i],
+            }
             emb[i] = curDict;
         }
-        this.setState({emb: emb});
+        this.setState({emb: emb, data: data}, ()=> console.log(this.state.emb));
+    }
+
+    shouldComponentUpdate(prevProps, nextState){
+        if(this.state.updated){
+            console.log("false");
+            return false;
+        }
+        console.log("true");
+        return true;
     }
 
     componentDidUpdate(prevProps){
         if(prevProps.nodes && this.props.nodes && !equals(prevProps.nodes, this.props.nodes)){
             this.generateData(this.props.nodes);
+            this.setState({updated: true});
         }
     }
     render() {
@@ -52,9 +71,31 @@ export default class SearchSpace extends Component {
                     // generate random number b/w 1 and max int -> that is the mask
                     this.props.callbackFromChild({hoverMask: this.state.emb[e.points[0].pointNumber].mask})
                 }}
+                onRelayout={(e) => {
+                    //To select points from the scatterplot which are then highlighted on the parallel coordinate plot
+                    if (!e["xaxis.autorange"]){
+                        let ymin = e["yaxis.range[0]"];
+                        let ymax = e["yaxis.range[1]"];
+                        let xmin = e["xaxis.range[0]"];
+                        let xmax = e["xaxis.range[1]"];
+                        //Filtering the points to retrieve the selected points
+                        let filteredArr = [];
+                        for(let i=0;i<this.state.data.length;i++){
+                            // console.log(this.state.embedArr[i].x)
+                            if((this.state.data[i][0] > xmin) && (this.state.data[i][0] < xmax) && (this.state.data[i][1] > ymin) && (this.state.data[i][1] < ymax)){
+                                filteredArr.push(this.state.emb[i]);
+                            }
+                        }
+                        console.log(filteredArr)
+                        //Changes state of app.js by sending selected points
+                        // this.props.callbackFromParent({'scatter_points': filteredArr, 'para_points': filteredArr});
+
+                    }
+                }}
 
                 layout={ {  
-                            uirevision: false, xaxis:{autorange: true}, 
+                            uirevision: false, 
+                            xaxis:{autorange: true}, 
                             yaxis: {autorange: true}, 
                             paper_bgcolor: "transparent",
                             plot_bgcolor: "transparent",
